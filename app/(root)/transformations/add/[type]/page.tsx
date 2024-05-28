5a3c51a36410820464bbc735a47989f6b822547e
Rahim Nasir <68578449+rahimnasir@users.noreply.github.com>
2024-05-29 03:28:46 +0800

    fix directory add page

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/shared/Header'
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <Header 
+      title="Transformation Title" 
+      subtitle="Transformation Subtitle"/>
+  )
+}
+
+export default AddTransformationTypePage
