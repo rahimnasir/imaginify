a75169757a6ef4039f0fc60e835a7c12e9f338b6
Rahim Nasir <68578449+rahimnasir@users.noreply.github.com>
2024-05-30 17:45:52 +0800

    add form input

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,11 +1,34 @@
 import Header from '@/components/shared/Header'
-import React from 'react'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({params:{type}}:SearchParamProps) => {
+  const {userId} = auth();
+  const transformation = transformationTypes[type];
+
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId)
+
   return (
-    <Header 
-      title="Transformation Title" 
-      subtitle="Transformation Subtitle"/>
+    <>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+
+
   )
 }
 
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -20,7 +20,7 @@ export const connectToDatabase = async () => {
 
   if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
   cached.promise = mongoose.connect(MONGODB_URL, {
-    dbName:'imaginify',bufferCommands:false
+    dbName:'imaginify',bufferCommands:true
   })
 
   cached.conn = await cached.promise;
